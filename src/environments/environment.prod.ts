09a4e543a540e41b115fb62e306f62dfa103d2b1
edutorname <edutorname@gmail.com>
2026-05-03 05:49:29 +0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   "discordId": "1444774799468531824",
-  "apiUrl": "https://camilo404.azurewebsites.net/v1/",
+  "apiUrl": "https://api.lanyard.rest/v1/users/",
   "webSocketUrl": "wss://api.lanyard.rest/socket"
 };
